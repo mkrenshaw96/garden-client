0139e798b930d700e03272b4ad8e44b6ce88806d
Michael Crenshaw <mkrenshaw96@gmail.com>
2019-08-13 12:49:49 -0400

    Added styling for the begening of the products page

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
+  products = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
   constructor() { }
 
   ngOnInit() {
